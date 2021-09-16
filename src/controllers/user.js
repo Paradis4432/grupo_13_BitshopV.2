530d175fc8e96c8c9c2e30f8441a6b3d4aa69c34
Paradis4432 <Lucasezequiel120202@gmail.com>
2021-09-15 21:53:16 -0300

    login working

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,3 @@
 module.exports = {
     login: (req, res) => res.render("users/login", { style: "login", title: "User | Login" }),
-    register: (req, res) => res.render("users/register", { style: "register", title: "User | Register" })
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const user = require("../controllers/user");
 
 router.get("/login", user.login);
-router.get("/register", user.register);
 
 module.exports = router;
